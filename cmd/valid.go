248f065c733a8bcb863271921fcca418d0831df1
DragonSSS <songweilong1989@gmail.com>
2020-09-25 21:54:08 -0400

    - Continue to init validate subcommand

--- cmd/valid.go
+++ cmd/valid.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"io/ioutil"
+
+	"github.com/dgrijalva/jwt-go"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -15,7 +18,38 @@ var (
 )
 
 func validateToken(pemPath, token string) {
+	publicBytes, err := ioutil.ReadFile(pemPath)
+	if err != nil {
+		log.WithError(err).Fatalf("Failed to read pem file from the path %s", pemPath)
+	}
 
+	publicKey, err := jwt.ParseRSAPublicKeyFromPEM(publicBytes)
+	if err != nil {
+		log.WithError(err).Fatalf("Failed to convert pem file to rsa public key from the path %s", pemPath)
+	}
+
+	validatedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		// since we only use the one private key to sign the tokens,
+		// we also only use its public counter part to verify
+		return publicKey, nil
+	})
+
+	switch err.(type) {
+	case nil:
+		if !validatedToken.Valid {
+			return
+		}
+	case *jwt.ValidationError: // something was wrong during the validation
+		vErr := err.(*jwt.ValidationError)
+		switch vErr.Errors {
+		case jwt.ValidationErrorExpired:
+			return
+		default:
+			return
+		}
+	default: // something else went wrong
+		return
+	}
 }
 
 func validate(cmd *cobra.Command, args []string) {
